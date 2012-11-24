6299a7d8cf494c1d6029e0621d105f75140951b3
iceandfire <arhamjamal@gmail.com>
2012-11-24 12:23:04 +0530

    Added new boot animation and removed overlays for now. Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JZO54K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JOP40C BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
