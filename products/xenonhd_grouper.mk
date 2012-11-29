ce1a68bb104d26753470cb453db8c3cf113948c7
iceandfire <arhamjamal@gmail.com>
2012-11-30 01:05:26 +0530

    Update build fingerprint for grouper

--- products/xenonhd_grouper.mk
+++ products/xenonhd_grouper.mk
@@ -11,7 +11,7 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.1.2/JZO54K/485486:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.1.2 JZO54K 485486 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2/JOP40C/527662:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2 JOP40C 527662 release-keys"
 
 # Inherit common build.prop overrides
 -include vendor/xenonhd/products/common_versions.mk
