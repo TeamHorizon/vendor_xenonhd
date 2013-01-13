rascarlo/android_vendor_rascarlo

dictionaries and gesture input overlay

products/common.mk
@@ -18,6 +18,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/rascarlo/overlay/common
 
+# Common dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/rascarlo/overlay/dictionaries
+
 # Supersu
 PRODUCT_COPY_FILES += \
     vendor/rascarlo/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \

TeamHorizon/vendor_xenonhd

Conflicts:
	products/common.mk
Signed-off-by: iceandfire <arhamjamal@gmail.com>

products/common.mk
@@ -23,6 +23,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.ril.disable.power.collapse=1 \
 	pm.sleep_mode=1
 
+# Common dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/xenonhd/overlay/dictionaries
+
 # Blobs common to all devices
 PRODUCT_COPY_FILES += \
     vendor/xenonhd/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \