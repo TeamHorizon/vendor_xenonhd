8e37183b97615b60a56311069d07b5540f18a55f
iceandfire <arhamjamal@gmail.com>
2013-03-26 13:52:36 +0530

    I might be crazy
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- products/xenonhd_toro.mk
+++ products/xenonhd_toro.mk
@@ -11,7 +11,7 @@ PRODUCT_DEVICE := toro
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_FINGERPRINT=google/mysid/toro:4.1.1/JRO03O/424425:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.1.1 JRO03O 424425 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mysid BUILD_FINGERPRINT="google/mysid/toro:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="mysid-user 4.2.2 JDQ39 573038 release-keys"
 
 # Inherit common build.prop overrides
 -include vendor/xenonhd/products/common_versions.mk
