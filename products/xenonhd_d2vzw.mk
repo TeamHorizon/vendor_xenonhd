e0b051a7b3d593ccebb3df5803a7415a002a8f74
TheBr0ken <jtoro2716@gmail.com>
2013-05-27 10:13:56 -0500

    Update xenonhd_d2vzw.mk

--- products/xenonhd_d2vzw.mk
+++ products/xenonhd_d2vzw.mk
@@ -11,8 +11,6 @@ PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMB1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMB1 release-keys"
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/d2-common
-
 # Copy d2 specific prebuilt files
 PRODUCT_COPY_FILES +=  \
     vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
