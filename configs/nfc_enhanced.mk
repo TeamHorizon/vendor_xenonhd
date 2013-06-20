0e7f6b7158d7ab932fbdef87aaccc8c3f018f665
TheBr0ken <jtoro2716@gmail.com>
2013-06-20 11:01:15 -0400

    Fix Typo
    Thanks Ntemis for catching this!

--- configs/nfc_enhanced.mk
+++ configs/nfc_enhanced.mk
@@ -1,3 +1,3 @@
 # This device supports CM enhanced NFC
 PRODUCT_COPY_FILES += \
-    vendor/xenonhd/config/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
+    vendor/xenonhd/configs/permissions/com.cyanogenmod.nfc.enhanced.xml:system/etc/permissions/com.cyanogenmod.nfc.enhanced.xml
