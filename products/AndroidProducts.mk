626b6a06f507f2086f3f0b3b4a72ece19ed6d050
iceandfire <arhamjamal@gmail.com>
2013-06-29 03:46:24 +0530

    Organize lunch menu alphabetically

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -5,28 +5,28 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_d2usc.mk \
 	$(LOCAL_DIR)/xenonhd_d2vzw.mk \
 	$(LOCAL_DIR)/xenonhd_e975.mk \
+	$(LOCAL_DIR)/xenonhd_endeavoru.mk \
+	$(LOCAL_DIR)/xenonhd_find5.mk \
 	$(LOCAL_DIR)/xenonhd_galaxysmtd.mk \
 	$(LOCAL_DIR)/xenonhd_grouper.mk \
-	$(LOCAL_DIR)/xenonhd_endeavoru.mk \
-	$(LOCAL_DIR)/xenonhd_i9100.mk\
-    $(LOCAL_DIR)/xenonhd_i9100g.mk\
-    $(LOCAL_DIR)/xenonhd_i9300.mk\
-    $(LOCAL_DIR)/xenonhd_i9305.mk\
+	$(LOCAL_DIR)/xenonhd_i9100.mk \
+    $(LOCAL_DIR)/xenonhd_i9100g.mk \
+    $(LOCAL_DIR)/xenonhd_i9300.mk \
+    $(LOCAL_DIR)/xenonhd_i9305.mk \
+    $(LOCAL_DIR)/xenonhd_jfltevzw.mk \
+    $(LOCAL_DIR)/xenonhd_m7.mk \
 	$(LOCAL_DIR)/xenonhd_maguro.mk \
 	$(LOCAL_DIR)/xenonhd_mako.mk \
-	$(LOCAL_DIR)/xenonhd_tilapia.mk \
+	$(LOCAL_DIR)/xenonhd_mb886.mk \
+	$(LOCAL_DIR)/xenonhd_n7000.mk \
+	$(LOCAL_DIR)/xenonhd_n7100.mk \
 	$(LOCAL_DIR)/xenonhd_t0lte.mk \
 	$(LOCAL_DIR)/xenonhd_t0lteatt.mk \
 	$(LOCAL_DIR)/xenonhd_t0ltetmo.mk \
+	$(LOCAL_DIR)/xenonhd_tilapia.mk \
 	$(LOCAL_DIR)/xenonhd_toro.mk \
 	$(LOCAL_DIR)/xenonhd_toroplus.mk \
-	$(LOCAL_DIR)/xenonhd_vigor.mk\
-    $(LOCAL_DIR)/xenonhd_jfltevzw.mk\
-	$(LOCAL_DIR)/xenonhd_xt925.mk\
-	$(LOCAL_DIR)/xenonhd_xt926.mk\
-	$(LOCAL_DIR)/xenonhd_n7000.mk\
-	$(LOCAL_DIR)/xenonhd_n7100.mk\
-    $(LOCAL_DIR)/xenonhd_mb886.mk \
-    $(LOCAL_DIR)/xenonhd_m7.mk \
-    $(LOCAL_DIR)/xenonhd_xt907.mk \
-    $(LOCAL_DIR)/xenonhd_find5.mk
+	$(LOCAL_DIR)/xenonhd_vigor.mk \
+	$(LOCAL_DIR)/xenonhd_xt925.mk \
+	$(LOCAL_DIR)/xenonhd_xt926.mk \
+    $(LOCAL_DIR)/xenonhd_xt907.mk
