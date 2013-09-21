612adb45b34f1e1bbf435119fe7f5f74f8d91c3d
iceandfire <arhamjamal@gmail.com>
2013-09-22 01:50:44 +0530

    Remove tweaks for now
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- products/common.mk
+++ products/common.mk
@@ -76,10 +76,8 @@ PRODUCT_COPY_FILES += \
     vendor/xenonhd/proprietary/common/etc/init.d/00check:system/etc/init.d/00check \
     vendor/xenonhd/proprietary/common/etc/init.local.rc:root/init.xenonhd.rc \
     vendor/xenonhd/proprietary/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-    vendor/xenonhd/proprietary/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
     vendor/xenonhd/proprietary/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
     vendor/xenonhd/proprietary/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
-    vendor/xenonhd/proprietary/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
     vendor/xenonhd/proprietary/common/etc/init_trigger.enabled:system/etc/init_trigger.enabled \
     vendor/xenonhd/proprietary/common/etc/sysctl.conf:system/etc/sysctl.conf \
     vendor/xenonhd/proprietary/common/bin/sysinit:system/bin/sysinit
