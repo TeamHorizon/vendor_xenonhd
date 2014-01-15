e3c695f06789936ef0b80f18e5aab033c809568a
iceandfire <arhamjamal@gmail.com>
2014-01-15 16:28:59 +0530

    Let's try this once more
    Change-Id: I2afbbeaf175021cea8e90bf553020d511773d3cb
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JLS36I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
