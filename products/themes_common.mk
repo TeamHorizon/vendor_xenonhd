7b3d43cd600d137eff15a906503418618255f934
Peter Carenza <pcarenza@gmail.com>
2014-04-29 12:47:42 -0400

    CM11 Themes: Add feature for new themes.
    Change-Id: I0da30606534c13a43de33bef08f79fbbb1b348d3
    Author: Clark Sheff

--- products/themes_common.mk
+++ products/themes_common.mk
@@ -5,3 +5,4 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
        vendor/xenonhd/proprietary/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/xenonhd/proprietary/common/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
