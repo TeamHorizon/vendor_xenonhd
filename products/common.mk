RAZR-K-Devs/android_vendor_cm

Enable gesture typing - (2/3) - add prebuilt lib
Change-Id: Ia8d90975c4aa842269f2f3fcd1b8decf90a48b47

Conflicts:
	config/common.mk

config/common.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
+# Copy latinime for gesture typing
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+
 # SELinux filesystem labels
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel

TeamHorizon/vendor_xenonhd

Signed-off-by: Peter Carenza <pcarenza@gmail.com>

products/common.mk
@@ -120,6 +120,7 @@ PRODUCT_COPY_FILES += \
     vendor/xenonhd/proprietary/common/etc/init.d/97fixgestures:system/etc/init.d/97fixgestures \
     vendor/xenonhd/proprietary/common/etc/init_trigger.enabled:system/etc/init_trigger.enabled \
     vendor/xenonhd/proprietary/common/etc/sysctl.conf:system/etc/sysctl.conf \
+    vendor/xenonhd/proprietary/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
     vendor/xenonhd/proprietary/common/bin/sysinit:system/bin/sysinit
 
 # SELinux filesystem labels