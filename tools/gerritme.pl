3fdc34bf10b76534ff238160fac36c94502e96cd
Peter Carenza <pcarenza@gmail.com>
2014-05-02 23:34:46 -0400

    Add gerrit tool for our benefit
    Simple to run - just go into one of our directories with a th remote already established
    Run this script, it will create a gerrit remote as well as default reviewers and set up the default push to kitkat
    So all you have to do for each commit is "git push gerrit" and voila!
    
    Caveat - this is a one-time-only deal for each directory with a git. Things break after the first time you do this.
    Z
    
    Change-Id: Ic436d7d184f60675d97b7b8ba45458ef7fae100e

---
+++ tools/gerritme.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+# git remote add gerrit ssh://pcarenza@review.gerrithub.io:29418/TeamHorizon/android_packages_apps_Gallery2
+# gitconfig
+#[remote "gerrit"]
+#        url = ssh://pcarenza@review.gerrithub.io:29418/TeamHorizon/android_build
+#        fetch = +refs/heads/*:refs/remotes/gerrit/*
+#        push = HEAD:refs/for/kitkat
+#        receivepack = git receive-pack --reviewer jtoro2716@gmail.com --reviewer arhamjamal@gmail.com
+#
+# git config remote.gerrit.url "ssh://pcarenza@review.gerrithub.io:29418/TeamHorizon/android_build"
+# git config remote.gerrit.push "HEAD:refs/for/kitkat"
+# git config remote.gerrit.receivepack "git receive-pack --reviewer jtoro2716@gmail.com --reviewer arhamjamal@gmail.com"
+# Fetch URL: https://github.com/TeamHorizon/platform_frameworks_base
+
+@reviewers = ("jtoro271@gmail.com","arhamjamal@gmail.com","pcarenza@gmail.com");
+chomp ($username = `whoami`);
+
+chomp ($branch = `git branch| grep "*"`);
+$branch =~ s/\*\s//g;
+
+chomp ($repo = `git remote show th | grep "Fetch URL"`);
+$repo =~ s/.*Fetch URL: https:\/\/github.com\/TeamHorizon\///g;
+
+print "**** Setting up gerrit configuration for $repo. ****\n";
+
+$basecommand = 'git config remote.gerrit.';
+
+$base{'url'} = "ssh://$username\@review.gerrithub.io:29418/TeamHorizon/$repo";
+
+$base{'push'} = "HEAD:refs/for/kitkat";
+
+$base{'receivepack'} = "git receive-pack";
+
+$reviewertitle = "--reviewer ";
+
+print "branch is $branch\n";
+$addcommand = "git remote add gerrit $base{'url'}";
+print "$addcommand\n";
+
+foreach my $key (keys %base) {
+     $command= "$basecommand$key \"$base{$key}\"";
+     print "$command\n";
+     `$command`;     
+}
+
