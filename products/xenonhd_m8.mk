66281e075727e086e49ceecedfde8ae4c2291423
iceandfire <arhamjamal@gmail.com>
2014-11-09 22:03:34 +0530

    Bring in HTC One M8
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

---
+++ products/xenonhd_m8.mk
@@ -0,0 +1,25 @@
+# Inherit AOSP device configuration for m8.
+$(call inherit-product, device/htc/m8/full_m8.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
+
+# Copy m8 specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
+# Inherit drm blobs
+-include vendor/xenonhd/products/common_drm.mk
+
+# Setup device specific product configuration.
+PRODUCT_NAME := xenonhd_m8
+PRODUCT_BRAND := htc
+PRODUCT_DEVICE := m8
+PRODUCT_MODEL := One M8
+PRODUCT_MANUFACTURER := HTC
