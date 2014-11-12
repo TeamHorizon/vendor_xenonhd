5c8b43591869eba798248d27893bd1dadb7cb5ac
Joe Maples <frap129@gmail.com>
2014-11-11 19:09:53 -0500

    Fix d2lte and cleanup

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,9 +1,6 @@
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_crespo.mk \
-	$(LOCAL_DIR)/xenonhd_d2att.mk \
-	$(LOCAL_DIR)/xenonhd_d2tmo.mk \
-	$(LOCAL_DIR)/xenonhd_d2usc.mk \
-	$(LOCAL_DIR)/xenonhd_d2vzw.mk \
+	$(LOCAL_DIR)/xenonhd_d2lte.mk \
 	$(LOCAL_DIR)/xenonhd_e975.mk \
 	$(LOCAL_DIR)/xenonhd_endeavoru.mk \
 	$(LOCAL_DIR)/xenonhd_find5.mk \
@@ -12,21 +9,21 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_grouper.mk \
 	$(LOCAL_DIR)/xenonhd_hammerhead.mk \
 	$(LOCAL_DIR)/xenonhd_honami.mk \
-  $(LOCAL_DIR)/xenonhd_i605.mk \
+	$(LOCAL_DIR)/xenonhd_i605.mk \
 	$(LOCAL_DIR)/xenonhd_i9100.mk \
-  $(LOCAL_DIR)/xenonhd_i9100g.mk \
-  $(LOCAL_DIR)/xenonhd_i9300.mk \
-  $(LOCAL_DIR)/xenonhd_i9305.mk \
-  $(LOCAL_DIR)/xenonhd_jflte.mk \
-  $(LOCAL_DIR)/xenonhd_l900.mk \
+	$(LOCAL_DIR)/xenonhd_i9100g.mk \
+	$(LOCAL_DIR)/xenonhd_i9300.mk \
+	$(LOCAL_DIR)/xenonhd_i9305.mk \
+	$(LOCAL_DIR)/xenonhd_jflte.mk \
+	$(LOCAL_DIR)/xenonhd_l900.mk \
 	$(LOCAL_DIR)/xenonhd_m8.mk \
-  $(LOCAL_DIR)/xenonhd_maguro.mk \
+	$(LOCAL_DIR)/xenonhd_maguro.mk \
 	$(LOCAL_DIR)/xenonhd_mako.mk \
 	$(LOCAL_DIR)/xenonhd_mb886.mk \
-  $(LOCAL_DIR)/xenonhd_m7tmo.mk \
+	$(LOCAL_DIR)/xenonhd_m7tmo.mk \
 	$(LOCAL_DIR)/xenonhd_n7000.mk \
 	$(LOCAL_DIR)/xenonhd_n7100.mk \
-  $(LOCAL_DIR)/xenonhd_r950.mk \
+	$(LOCAL_DIR)/xenonhd_r950.mk \
 	$(LOCAL_DIR)/xenonhd_t0lte.mk \
 	$(LOCAL_DIR)/xenonhd_t0lteatt.mk \
 	$(LOCAL_DIR)/xenonhd_t0ltetmo.mk \
@@ -36,4 +33,4 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_vigor.mk \
 	$(LOCAL_DIR)/xenonhd_xt925.mk \
 	$(LOCAL_DIR)/xenonhd_xt926.mk \
-  $(LOCAL_DIR)/xenonhd_xt907.mk
+	$(LOCAL_DIR)/xenonhd_xt907.mk
