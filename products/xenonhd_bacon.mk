a5587ef1142fef280016ef288b361c2fe43b24bd
pcarenza <pcarenza@gmail.com>
2015-01-03 10:18:45 -0500

    vendor mk for bacon - initial commit CM params

---
+++ products/xenonhd_bacon.mk
@@ -0,0 +1,30 @@
+# Inherit AOSP device configuration for bacon.
+$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
+
+# Copy hammerhead specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
+# Inherit drm blobs
+-include vendor/xenonhd/products/common_drm.mk
+
+# Setup device specific product configuration.
+PRODUCT_NAME := xenonhd_bacon
+PRODUCT_BRAND := oneplus
+PRODUCT_DEVICE := bacon
+PRODUCT_MODEL := A001
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=bacon BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH25R:user/release-keys PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH25R release-keys"
