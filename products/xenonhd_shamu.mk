c204c839268be0e629c7af1b8326c4b2cd427380
iceandfire <arhamjamal@gmail.com>
2015-01-14 21:49:43 +0530

    Bring in shamu
    Change-Id: I7744627395065e1de38688f4127aef00035e0eff
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

---
+++ products/xenonhd_shamu.mk
@@ -0,0 +1,34 @@
+# Inherit AOSP device configuration for shamu.
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
+
+# Copy hammerhead specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
+# Inherit drm blobs
+-include vendor/xenonhd/products/common_drm.mk
+
+# APNs
+PRODUCT_COPY_FILES +=  \
+	vendor/xenonhd/proprietary/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := xenonhd_shamu
+PRODUCT_DEVICE := shamu
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 6
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=shamu \
+    BUILD_FINGERPRINT=google/shamu/shamu:5.0.1/LRX22C/1602158:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 5.0.1 LRX22C 1602158 release-keys"
