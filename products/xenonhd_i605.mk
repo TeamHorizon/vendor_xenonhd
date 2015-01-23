f17a43e5a8d21bc1631546ea77485098f2713bcd
TheBr0ken <jtoro2716@gmail.com>
2015-01-23 10:37:41 -0600

    Initial L Bringup

--- products/xenonhd_i605.mk
+++ products/xenonhd_i605.mk
@@ -1,11 +1,19 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/xenonhd/configs/vzw.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/samsung/i605/full_i605.mk)
-
-# Inherit common product files.
-$(call inherit-product, vendor/xenonhd/products/common.mk)
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2015 TeamHorizon - XenonHD Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Release name
 PRODUCT_RELEASE_NAME := i605
@@ -14,20 +22,23 @@ PRODUCT_RELEASE_NAME := i605
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/i605/full_i605.mk)
+
 # Inherit common build.prop overrides
 -include vendor/xenonhd/products/common_versions.mk
 
-# Copy maguro specific prebuilt files
+# Copy i605 specific prebuilt files
 PRODUCT_COPY_FILES +=  \
-	vendor/xenonhd/proprietary/hybrid/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
+    vendor/xenonhd/proprietary/hybrid/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
     vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
     vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
     vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
-# Inherit drm blobs
--include vendor/xenonhd/products/common_drm.mk
-    
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i605
 PRODUCT_NAME := xenonhd_i605
@@ -35,5 +46,7 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SCH-I605
 PRODUCT_MANUFACTURER := samsung
 
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.1.2/JZO54K/I605VRAMC3:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.1.2 JZO54K I605VRAMC3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.4.2/KOT49H/I605VRUFND7:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.4.2 KOT49H I605VRUFND7 release-keys"
