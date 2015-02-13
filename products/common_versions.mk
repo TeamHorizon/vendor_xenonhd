51bd10eb6b037804fd12d42b3431080acb5bc5fc
TheBr0ken <jtoro2716@gmail.com>
2015-02-13 18:53:09 -0500

    Add proper file naming [2/2]
    Change-Id: I672c39b84e2908cbc6de28045880d2aed39dda0f

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -1,6 +1,8 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+XENONHD_VERSION :=  update-XenonHD-$(shell date +"%m-%d-%y")-signed
+
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xehdversion=XenonHD-$(shell date +"%m-%d-%y") \
