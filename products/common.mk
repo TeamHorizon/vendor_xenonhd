AICP/vendor_aicp

Add SuperSU to AICP [1/2]
V2.36

Change-Id: I6bdce628301f0121baeaf3f2f8486065bae63cb7

configs/common.mk
@@ -160,6 +160,11 @@ PRODUCT_COPY_FILES += \
     vendor/aicp/prebuilt/etc/hosts.alt:system/etc/hosts.alt \
     vendor/aicp/prebuilt/etc/hosts.og:system/etc/hosts.og
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+    vendor/aicp/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/aicp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # OTA
 # PRODUCT_COPY_FILES +=  \
 #    vendor/aicp/proprietary/AICP_OTA.apk:system/priv-app/AICP_OTA.apk

TeamHorizon/vendor_xenonhd

Conflicts:
	configs/common.mk

products/common.mk
@@ -193,6 +193,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/xenonhd/proprietary/common/app/Layers.apk:system/app/Layers.apk
 
+# SuperSU
+PRODUCT_COPY_FILES += \
+     vendor/xenonhd/proprietary/common/app/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+     vendor/xenonhd/proprietary/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml