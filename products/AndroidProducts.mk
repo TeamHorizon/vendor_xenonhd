e60b4b4eb372635d31db7a35af7c0eccb0baabcd
iceandfire <arhamjamal@gmail.com>
2015-03-08 18:15:12 +0530

    Introduce hammerhead-caf
    Change-Id: Iba96e608cdfc3f0737e3c6a64396b3c345074bd7
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -14,6 +14,7 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_galaxysmtd.mk \
 	$(LOCAL_DIR)/xenonhd_grouper.mk \
 	$(LOCAL_DIR)/xenonhd_hammerhead.mk \
+	$(LOCAL_DIR)/xenonhd_hammerheadcaf.mk \
 	$(LOCAL_DIR)/xenonhd_honami.mk \
 	$(LOCAL_DIR)/xenonhd_i605.mk \
 	$(LOCAL_DIR)/xenonhd_i9100.mk \
---
+++ products/xenonhd_hammerheadcaf.mk
@@ -0,0 +1,30 @@
+# Inherit AOSP device configuration for hammerhead-caf.
+$(call inherit-product, device/lge/hammerheadcaf/full_hammerhead.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Telephony
+$(call inherit-product, vendor/xenonhd/configs/telephony.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
+
+# Copy hammerhead specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
+    vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+
+# Inherit drm blobs
+-include vendor/xenonhd/products/common_drm.mk
+
+# Setup device specific product configuration.
+PRODUCT_NAME := xenonhd_hammerheadcaf
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := hammerhead
+PRODUCT_MODEL := Nexus 5
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
