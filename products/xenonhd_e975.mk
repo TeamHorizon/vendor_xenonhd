0d78403c7c758b79d438d93e7df2a17607b646c0
Daniel Sikorski <dadi.sikorski@gmail.com>
2015-05-13 21:59:20 +0200

    Update xenonhd_e975.mk

--- products/xenonhd_e975.mk
+++ products/xenonhd_e975.mk
@@ -1,3 +1,7 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
 # Inherit AOSP device configuration for one x.
 $(call inherit-product, device/lge/e975/e975.mk)
 
@@ -24,7 +28,13 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_DEVICE := e975
 PRODUCT_NAME := xenonhd_e975
 PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-E975
+PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc_open_eu BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.1.2/JZO54K/E97510b.1360741472:user/release-keys PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.1.2 JZO54K E97510b.1360741472 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=geehrc_open_eu \
+	BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.1.2/JZO54K/E97510b.1360741472:user/release-keys \
+	PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.1.2 JZO54K E97510b.1360741472 release-keys"
+	
+# Enable Torch
+PRODUCT_PACKAGES += Torch
