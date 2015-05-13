6916eb84185e7b81e303f03ac79dfd92b07a6e9e
audahadi <wan.audahadi@gmail.com>
2015-05-14 01:38:53 +0800

    add cancro (xiaomi mi3) to lunch combo

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,6 +1,7 @@
 PRODUCT_MAKEFILES := \
         $(LOCAL_DIR)/xenonhd_bacon.mk \
 	$(LOCAL_DIR)/xenonhd_crespo.mk \
+	$(LOCAL_DIR)/xenonhd_cancro.mk \
 	$(LOCAL_DIR)/xenonhd_d2spr.mk \
 	$(LOCAL_DIR)/xenonhd_d2bst.mk \
 	$(LOCAL_DIR)/xenonhd_d2att.mk \
