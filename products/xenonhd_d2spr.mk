aee2e41d716d2a703742d6d60d7d7fe4303aa92c
Joe Maples <frap129@gmail.com>
2015-05-14 16:50:19 -0400

    Update d2spr config
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- products/xenonhd_d2spr.mk
+++ products/xenonhd_d2spr.mk
@@ -8,6 +8,11 @@ PRODUCT_DEVICE := d2spr
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := d2spr
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=d2spr \
+    BUILD_FINGERPRINT="samsung/d2spr/d2spr:5.0.2/KOT49H/L710VPUDNJ2:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2spr-user 5.0.2 KOT49H L710VPUDNJ2 release-keys"
+
 
 # Copy d2 specific prebuilt files
 PRODUCT_COPY_FILES +=  \
@@ -22,3 +27,4 @@ PRODUCT_COPY_FILES +=  \
 
 # Inherit drm blobs
 -include vendor/xenonhd/products/common_drm.mk
+lp-mr
