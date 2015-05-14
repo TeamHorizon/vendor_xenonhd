bb77de776165d077531f099fbfc1d61ba6a44582
TheBr0ken <jtoro2716@gmail.com>
2015-05-14 19:16:26 -0400

    Add back d2att/d2tmo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,9 @@
 add_lunch_combo xenonhd_bacon-userdebug
 add_lunch_combo xenonhd_crespo-userdebug
 add_lunch_combo xenonhd_cancro-userdebug
+add_lunch_combo xenonhd_d2att-userdebug
 add_lunch_combo xenonhd_d2spr-userdebug
+add_lunch_combo xenonhd_d2tmo-userdebug
 add_lunch_combo xenonhd_d2vzw-userdebug
 add_lunch_combo xenonhd_e975-userdebug
 add_lunch_combo xenonhd_endeavoru-userdebug
