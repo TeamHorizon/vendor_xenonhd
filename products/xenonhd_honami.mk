d020976630d4bcef507efadc61d4137528cac3ad
Gerald <geraldcarvalho70@gmail.com>
2015-06-29 11:57:35 +0530

    added Telephony config and updated FINGERPRINT
    Signed-off-by: geralD-carl <geraldcarvalho70@gmail.com>

--- products/xenonhd_honami.mk
+++ products/xenonhd_honami.mk
@@ -4,6 +4,9 @@ $(call inherit-product, device/sony/honami/full_honami.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/xenonhd/products/common.mk)
 
+# Telephony
+$(call inherit-product, vendor/xenonhd/configs/telephony.mk)
+
 # Inherit common build.prop overrides
 -include vendor/xenonhd/products/common_versions.mk
 
@@ -28,4 +31,4 @@ PRODUCT_DEVICE := honami
 PRODUCT_MODEL := Xperia Z1
 PRODUCT_MANUFACTURER := Sony
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.3/14.2.A.0.290/eng.hudsonslave:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.3 RHINE-1.1-131125-1201 eng.hudsonslave test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/C6903/C6903:5.0.2/14.5.A.0.270/3750474323:user/release-keys PRIVATE_BUILD_DESC="C6903-user 5.0.2 14.5.A.0.270 3750474323 release-keys"
