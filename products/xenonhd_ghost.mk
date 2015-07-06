6c9812578fde1b761dee870e6b0df7a1d7ecf25f
Omkar <djomi400@gmail.com>
2015-07-06 11:32:13 +0530

    update xenonhd_ghost.mk

--- products/xenonhd_ghost.mk
+++ products/xenonhd_ghost.mk
@@ -1,10 +1,45 @@
-$(call inherit-product, device/motorola/ghost/full_ghost.mk)
+#
+# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2015 TeamHorizon - XenonHD Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit some common CM stuff.
+# Release name
+PRODUCT_RELEASE_NAME := ghost
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Default Alarm Sound
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.alarm_alert=Cesium.ogg
+
+# Inherit common product files.
 $(call inherit-product, vendor/xenonhd/products/common.mk)
+
+# Telephony
+$(call inherit-product, vendor/xenonhd/configs/telephony.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/xenonhd/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/ghost/full_ghost.mk)
+
+# Inherit common build.prop overrides
+-include vendor/xenonhd/products/common_versions.mk
 
 # Copy ghost specific prebuilt files
 PRODUCT_COPY_FILES +=  \
@@ -12,9 +47,6 @@ PRODUCT_COPY_FILES +=  \
     vendor/xenonhd/proprietary/tuna/media/audio/notifications/Nexus.mp3:system/media/audio/notifications/Nexus.mp3 \
     vendor/xenonhd/proprietary/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/xenonhd/proprietary/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
-
-# Inherit drm blobs
--include vendor/xenonhd/products/common_drm.mk
 
 # Setup device specific product configuration.
 PRODUCT_NAME := xenonhd_ghost
