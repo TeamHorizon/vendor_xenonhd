ae6d90ab0e73457e57fe61d0e3f714b61f832fa6
T-Macgnolia <tmacgnolia@gmail.com>
2015-07-22 18:20:48 -0400

    Update AndroidProducts.mk
    Redefined  c50 to h345 to be more device specific.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -18,6 +18,7 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_geehrc.mk \
 	$(LOCAL_DIR)/xenonhd_ghost.mk \
 	$(LOCAL_DIR)/xenonhd_grouper.mk \
+	$(LOCAL_DIR)/xenonhd_h345,mk \
 	$(LOCAL_DIR)/xenonhd_hammerhead.mk \
 	$(LOCAL_DIR)/xenonhd_hammerheadcaf.mk \
 	$(LOCAL_DIR)/xenonhd_honami.mk \
@@ -28,7 +29,6 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/xenonhd_i9305.mk \
 	$(LOCAL_DIR)/xenonhd_jflte.mk \
 	$(LOCAL_DIR)/xenonhd_jfltetmo.mk \
-	$(LOCAL_DIR)/xenonhd_c50.mk \
 	$(LOCAL_DIR)/xenonhd_l900.mk \
 	$(LOCAL_DIR)/xenonhd_m7.mk \
 	$(LOCAL_DIR)/xenonhd_m8.mk \
