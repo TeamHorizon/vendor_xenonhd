cecc92edea51a32c66aa4c5c770fc20c485cba7f
Daniel Sikorski <dadi.sikorski@gmail.com>
2015-07-23 20:27:56 +0200

    Add D855 - LG G3

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -8,6 +8,7 @@ PRODUCT_MAKEFILES := \
  	$(LOCAL_DIR)/xenonhd_d2tmo.mk \
  	$(LOCAL_DIR)/xenonhd_d2usc.mk \
  	$(LOCAL_DIR)/xenonhd_d2vzw.mk \
+ 	$(LOCAL_DIR)/xenonhd_d855.mk \
 	$(LOCAL_DIR)/xenonhd_e975.mk \
 	$(LOCAL_DIR)/xenonhd_endeavoru.mk \
 	$(LOCAL_DIR)/xenonhd_falcon.mk \
