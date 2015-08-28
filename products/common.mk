b6616390a229f61382b374d103f2eedef8d8a149
TheBr0ken <jtoro2716@gmail.com>
2015-08-28 14:08:38 -0400

    OTA: Add OTA support
    A few things are needed to get this working properly. You will need
    a flag added to your ~/.bashrc to set the build type. This has
    been setup specifically for XenonHD server build paths, so some
    modification will be needed for others.
    
    The flags you need is:
    
    export OTA_TYPE=
    
    This tells us what type of build this will be. XenonHD's server uses
    build type as part of the download zip path.
    At this time, usable values are:
    
    experimental
    weeklies
    stable
    
    If no value is set, a default of
    
    export OTA_TYPE=experimental
    
    will be used.
    
    XenonOTA also requires two files, ota_conf which is used by the app to read
    currently installed build info. The other is ota_lollipop.xml which is
    used to read the most current build available for download on the server.
    Both of these are automatically generated and no interaction is needed for
    ota_conf. ota_lollipop.xml will be added to your out dir. You will
    need to move this to a location that will be static. XenonOTA uses this info
    to compare what you have vs what is the latest build. For XenonHD we move
    this xml to ftp/OTA/<device>, then we move the zips as normal to the
    correct location. For example, we move an experimental i605 build to
    ftp/experimental/i605. The xml file that is in ftp/OTA/i605 lists build
    version and location path of the build download. If this is newer than
    what you currently have, it will display an update notification and you
    will be able to download the file zip and md5 with the provided dynamic
    links.
    
    ota_lollipop.xml has to be hosted at a location where the url doesnt change
    and a location that will auto download the xml file vs just opening it to
    read. Not all hosts allow this.
    
    Change-Id: Ie0240698156346a9c91fbde5c1b0c194ae2d4784

--- products/common.mk
+++ products/common.mk
@@ -23,7 +23,8 @@ PRODUCT_PACKAGES += \
     Torch \
     VoicePlus \
     KernelTweaker \
-    XenonWallpapers
+    XenonWallpapers \
+    XenonOTA
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
--- products/common_versions.mk
+++ products/common_versions.mk
@@ -16,8 +16,38 @@ ro.romstats.name=XenonHD \
 ro.romstats.version=$(shell date +"%m-%d-%y") \
 ro.romstats.tframe=2
 
-#OTA updater app
+
+# OTA
+ifeq ($(OTA_TYPE),)
+OTA_TYPE=experimental
+endif
+
+# Create ota_conf, Rudimentary, but it works for now
+$(shell echo -n "" > $(ANDROID_BUILD_TOP)/ota_conf)
+
+$(shell echo -e "#       **** This_is_an_auto-generated_configuration_file_used_by_XenonOTA****\n \
+\n \
+# OTA_build_product\n \
+ota-device=$(TARGET_PRODUCT)\n \
+\n \
+# Build_Location\n \
+ota_url=https://mirrors.c0urier.net/android/teamhorizon/OTA/$(TARGET_PRODUCT)/ota_lollipop.xml\n \
+\n \
+# release_type. This value never changes and is not build type dependent\n \
+release_type=Stable\n \
+\n \
+# Build.prop\n \
+device_name=ro.ota.device\n \
+version_source=ro.ota.version\n \
+\n \
+# Build_identifier\n \
+version_delimiter=-\n \
+version_position=2\n \
+version_format=yyyyMMdd" > $(ANDROID_BUILD_TOP)/ota_conf)
+
+$(shell sed -i "s/\( \|xenonhd_\)//g" $(ANDROID_BUILD_TOP)/ota_conf)
+
 PRODUCT_PROPERTY_OVERRIDES += \
-otaupdater.otaid=XenonHD_ROM \
-otaupdater.otaversion=$(shell date +"%Y%m%d") \
-otaupdater.otatime=$(shell date +"%Y%m%d-%H%M") \
+    ro.ota.device=$(shell sed -n -e'/ota-device/s/^.*=//p' $(ANDROID_BUILD_TOP)/ota_conf) \
+    ro.ota.type=$(OTA_TYPE) \
+    ro.ota.version=$(OTA_TYPE)-XenonHD-$(shell date +"%Y%m%d")
