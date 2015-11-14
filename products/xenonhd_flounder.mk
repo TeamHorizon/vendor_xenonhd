e5c7871386672f673824d25acc1bb6b052ad2c8a
iceandfire <arhamjamal@gmail.com>
2015-11-14 23:03:33 +0530

    Flounder: cleanup
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- products/xenonhd_flounder.mk
+++ products/xenonhd_flounder.mk
@@ -10,15 +10,6 @@ $(call inherit-product, vendor/xenonhd/products/common.mk)
 # Inherit vendor
 $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 
-# Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.8/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_SOURCE := kernel/htc/flounder
-TARGET_KERNEL_CONFIG := flounder_defconfig
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
-TARGET_PREBUILT_KERNEL := false
-
 # Copy shamu specific prebuilt files
 PRODUCT_COPY_FILES +=  \
     vendor/xenonhd/proprietary/tuna/media/bootanimation.zip:system/media/bootanimation.zip \
@@ -34,9 +25,9 @@ ADDITIONAL_BUILD_PROPERTIES += \
     persist.sys.isUsbOtgEnabled=true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY48I/2074855:user/release-keys \
-    PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY48I 2074855 release-keys"
+    PRODUCT_NAME=volantis \
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0/MRA58N/2289998:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0 MRA58N 2289998 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := xenonhd_flounder
