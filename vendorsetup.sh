5b8fa47226c3150af98db4134ced8cd304deacbc
ljjehl <ljjehl@gmail.com>
2016-12-26 23:40:42 +0100

    welcome m8d to XeHD

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,5 +8,6 @@ add_lunch_combo xenonhd_grouper-userdebug
 add_lunch_combo xenonhd_kenzo-userdebug
 add_lunch_combo xenonhd_m7-userdebug
 add_lunch_combo xenonhd_m8-userdebug
+add_lunch_combo xenonhd_m8d-userdebug
 add_lunch_combo xenonhd_mako-userdebug
 add_lunch_combo xenonhd_tomato-userdebug
