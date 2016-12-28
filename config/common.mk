44692c7057004d40044d81197310a412f463d353
Abhisek Devkota <ciwrl@lineageos.org>
2016-12-27 22:30:35 -0800

    Disable SU by default
    - We plan on distributing an addon for our SU
    - Developers that want SU by default can export
        WITH_SU=true to their env
    - This helps with application compatibility for
        various services, while maintaining user flexibility
    
    Change-Id: I6d52159676f805c3698ff59c896d943a3a4b25db

--- config/common.mk
+++ config/common.mk
@@ -240,8 +240,13 @@ PRODUCT_BOOT_JARS += \
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PACKAGES += \
     procmem \
-    procrank \
+    procrank
+
+# Conditionally build in su
+ifeq ($(WITH_SU),true)
+PRODUCT_PACKAGES += \
     su
+endif
 endif
 
 DEVICE_PACKAGE_OVERLAYS += $(OVERLAY)/common
