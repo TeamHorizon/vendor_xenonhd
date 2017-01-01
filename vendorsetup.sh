505a33ce68e6a05a29abb7e93cfa5b3462578024
SanjayVarun <sanjay.varun95@gmail.com>
2017-01-01 22:52:28 +0530

    xenonhd: Add onyx and titan
    Update README

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,4 +10,6 @@ add_lunch_combo xenonhd_m7-userdebug
 add_lunch_combo xenonhd_m8-userdebug
 add_lunch_combo xenonhd_m8d-userdebug
 add_lunch_combo xenonhd_mako-userdebug
+add_lunch_combo xenonhd_onyx-userdebug
+add_lunch_combo xenonhd_titan-userdebug
 add_lunch_combo xenonhd_tomato-userdebug
