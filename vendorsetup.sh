245a9eedd50b3603b53051526b18b14d1a36a98f
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-01-25 19:47:08 +0100

    xenonhd: Add lux to devices list (#77)
    :p

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,6 +7,7 @@ add_lunch_combo xenonhd_endeavoru-userdebug
 add_lunch_combo xenonhd_grouper-userdebug
 add_lunch_combo xenonhd_i9100-userdebug
 add_lunch_combo xenonhd_kenzo-userdebug
+add_lunch_combo xenonhd_lux-userdebug
 add_lunch_combo xenonhd_m7-userdebug
 add_lunch_combo xenonhd_m8-userdebug
 add_lunch_combo xenonhd_m8d-userdebug
