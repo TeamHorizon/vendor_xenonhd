1aba43dfb4e813d3b677b0b37e98fc4920d4016c
Olaia Segovia <olaia.segovia@bq.com>
2017-03-23 10:24:07 +0100

    (2/2) Make sensitive phone numbers not to be shown in call log history.
    Considering sensitive phone numbers to be, hotlines dealing with
    violence against women.
    In the EU, these numbers do not show up in the phone bill.
    In order to avoid these phone numbers to be listed in the Call Log, we
    have implemented a new XML file which is parsed with phone numbers
    from different countries to be filtered. This file needs to be copied to
    device via desired Android.mk file in order to be able to process it.
    The comparison is made checking the network MCC the SIM is connected to
    in order to consider roaming and multisim scenarios.
    
    Test: CallLogManagerTest.testDontLogCallsToSensitivePhoneNumber PASS
    
    Change-Id: I2e78d0f3e13d1ad1298613651ece136e30b86dfe
    Signed-off-by: Olaia Segovia <olaia.segovia@bq.com>

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,3 +1,7 @@
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    $(PREBUILT)/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
+
 # World APN list
 PRODUCT_COPY_FILES += \
     $(PREBUILT)/etc/apns-conf.xml:system/etc/apns-conf.xml
