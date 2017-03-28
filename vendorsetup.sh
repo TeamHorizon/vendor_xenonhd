f7c6354fc33e43584f22e019492ac9ffec118e46
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2017-03-28 18:15:47 +0200

    vendor: Add n7100 to the family

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,7 @@ add_lunch_combo xenonhd_m7-userdebug
 add_lunch_combo xenonhd_m8-userdebug
 add_lunch_combo xenonhd_m8d-userdebug
 add_lunch_combo xenonhd_mako-userdebug
+add_lunch_combo xenonhd_n7100-userdebug
 add_lunch_combo xenonhd_onyx-userdebug
 add_lunch_combo xenonhd_titan-userdebug
 add_lunch_combo xenonhd_tomato-userdebug
