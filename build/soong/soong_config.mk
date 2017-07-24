f65f81fe53f49f0f9b4aa82eec20c2cd69d24b9a
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:05:30 +0200

    soong_config: Add TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS
    * Soong product variable 'needs_text_relocations'
    
    Change-Id: Ia22c94922f37c49f0d66a67747efa0ee97b4e477
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -4,5 +4,6 @@ xenonhd_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"XenonHD": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
