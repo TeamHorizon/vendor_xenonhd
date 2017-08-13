292d0acec6a29cd0e2c724363bba459ef42f17b0
Ashwin Ramesh <ashwinr64@lineageos.org>
2017-08-13 12:00:15 +0530

    envsetup: Fix installboot on ZSH
    * ZSH errors out when it doesn't find anything in $OUT/system/lib/modules/.
    * So add a check to avoid error while using installboot() with a target
      that doesn't have modules enabled.
    
    Change-Id: Ibe66f6962943e9b90c7059a4c9b4ded558318326

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -316,12 +316,15 @@ function installboot()
     if (adb shell getprop ro.xenonhd.device | grep -q "$XENONHD_BUILD");
     then
         adb push $OUT/boot.img /cache/
-        for i in $OUT/system/lib/modules/*;
-        do
-            adb push $i /system/lib/modules/
-        done
+        if [ -e "$OUT/system/lib/modules/*" ];
+        then
+            for i in $OUT/system/lib/modules/*;
+            do
+                adb push $i /system/lib/modules/
+            done
+            adb shell chmod 644 /system/lib/modules/*
+        fi
         adb shell dd if=/cache/boot.img of=$PARTITION
-        adb shell chmod 644 /system/lib/modules/*
         echo "Installation complete."
     else
         echo "The connected device does not appear to be $XENONHD_BUILD, run away!"
