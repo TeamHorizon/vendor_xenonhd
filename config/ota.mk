50826ba423e2fcab3ea168e64d4b0901f2ec7fdb
dadi11 <dadi.sikorski@gmail.com>
2017-08-24 14:30:29 +0200

    xenonhd: switch to XenonHD versioning
    Change-Id: I007502c17093dfb3b682355921a84cc0cb34f0c9

---
+++ config/ota.mk
@@ -0,0 +1,28 @@
+# OTA default build type
+ifeq ($(OTA_TYPE),)
+OTA_TYPE=Unofficial
+endif
+
+# XenonHD version
+XENONHD_VERSION := XenonHD-$(shell date +"%y%m%d")-$(OTA_TYPE)
+DEVICE := $(subst xenonhd_,,$(TARGET_PRODUCT))
+
+# XenonHD OTA app
+PRODUCT_PACKAGES += \
+    XenonOTA
+
+# Build.prop overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.xenonhd.version=$(XENONHD_VERSION) \
+    ro.ota.device=$(DEVICE) \
+    ro.ota.type=$(OTA_TYPE)
+
+$(shell echo -e "# OTA_configuration\n \
+ota_url=https://mirrors.c0urier.net/android/teamhorizon/O/OTA/ota_$(DEVICE).xml\n \
+ota-device=$(DEVICE)\n \
+device_name=ro.ota.device\n \
+release_type=XenonHD\n \
+version_source=ro.xenonhd.version\n \
+version_delimiter=-\n \
+version_position=1\n \
+version_format=yyMMdd" > $(ANDROID_BUILD_TOP)/ota_conf)
