a554997d738e95ba7280950afd378f3b76bbd1af
dadi11 <dadi.sikorski@gmail.com>
2017-09-03 15:09:21 +0200

    xenonhd: disable OTA for unofficial builds
    * don't build XenonOTA app
    * don't create ota_conf
    * ota.xml won't be created without ota_conf as well
    
    Change-Id: Ic94cb41f4b3e1516d28f9ba494656283ba570336
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -7,15 +7,16 @@ endif
 XENONHD_VERSION := XenonHD-$(shell date +"%y%m%d")-$(OTA_TYPE)
 DEVICE := $(subst xenonhd_,,$(TARGET_PRODUCT))
 
-# XenonHD OTA app
-PRODUCT_PACKAGES += \
-    XenonOTA
-
 # Build.prop overrides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.xenonhd.version=$(XENONHD_VERSION) \
     ro.ota.device=$(DEVICE) \
     ro.ota.type=$(OTA_TYPE)
+
+ifneq ($(OTA_TYPE),Unofficial)
+# XenonHD OTA app
+PRODUCT_PACKAGES += \
+    XenonOTA
 
 $(shell echo -e "# OTA_configuration\n \
 ota_url=https://mirrors.c0urier.net/android/teamhorizon/O/OTA/ota_$(DEVICE).xml\n \
@@ -26,3 +27,4 @@ version_source=ro.xenonhd.version\n \
 version_delimiter=-\n \
 version_position=1\n \
 version_format=yyMMdd" > $(ANDROID_BUILD_TOP)/ota_conf)
+endif
