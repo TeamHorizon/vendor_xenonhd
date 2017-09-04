f4c74cb4c4c7dadbe059d3eec5fa8f405f2e01d7
dadi11 <dadi.sikorski@gmail.com>
2017-09-04 13:15:54 +0200

    xenonhd: restore accidentally removed include
    * just fixing my derp

--- config/common.mk
+++ config/common.mk
@@ -74,6 +74,7 @@ PRODUCT_COPY_FILES += \
 
 # Include OTA config, XenonHD audio files, Theme engine
 include $(CONFIG)/ota.mk
+include $(CONFIG)/themes_common.mk
 include $(CONFIG)/xenonhd_audio.mk
 
 # CMSDK
