40727fcf9f0a2164d5be554381614b43157c1af2
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put xenonhd variables in the "XenonHD" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the XenonHD structure
 xenonhd_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"XenonHD": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
