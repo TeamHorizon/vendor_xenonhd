d38bffa54acef98282ce8fc891e48e182c2c1109
BenJule <benlue@s3root.ovh>
2017-09-23 18:05:34 +0200

    XenonHD: Add Samsung jfltexx
    Change-Id: I860a17399bf9353e3edbd45e8a635e07d8f33770

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,4 @@ add_lunch_combo xenonhd_d855-userdebug
 add_lunch_combo xenonhd_gts210vewifi-userdebug
 add_lunch_combo xenonhd_lux-userdebug
 add_lunch_combo xenonhd_mido-userdebug
-
+add_lunch_combo xenonhd_jfltexx-userdebug
