69e8af7d8d09a91603b43145a04a40202b32052f
ljjehl <ljjehl@gmail.com>
2017-12-04 00:24:59 +0100

    Hello Nobleltetmo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -31,6 +31,7 @@ add_lunch_combo xenonhd_m7-userdebug
 add_lunch_combo xenonhd_m8-userdebug
 add_lunch_combo xenonhd_m8d-userdebug
 add_lunch_combo xenonhd_mako-userdebug
+add_lunch_combo xenonhd_nobleltetmo-userdebug
 add_lunch_combo xenonhd_n7100-userdebug
 add_lunch_combo xenonhd_onyx-userdebug
 add_lunch_combo xenonhd_titan-userdebug
