ea0ad3a3651ca16c0ebec59b46a34e360b6adcb2
Aaron Kling <webgeek1234@gmail.com>
2017-12-07 13:55:52 -0600

    ATV: Ship AppDrawer
    Also remove TvSettings from here since its included in atv_base
    
    Change-Id: I2d97e90e234dc9b7868dad3b063c0420211dacdc

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,6 @@
 # Inherit common XenonHD stuff
 $(call inherit-product, vendor/xenonhd/config/common_full.mk)
 
-PRODUCT_PACKAGES += TvSettings
+PRODUCT_PACKAGES += AppDrawer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/xenonhd/overlay/tv
