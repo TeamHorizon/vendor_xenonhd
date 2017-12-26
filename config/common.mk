d60954094c645bba7458660f20fcf394ce88c1bf
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-26 02:58:23 +0200

    xenonhd: Move all system props to PRODUCT_GENERIC_PROPERTIES
    For Treble enabled devices, PRODUCT_PROPERTY_OVERRIDES puts props
    added with that in /vendor/build.prop. While that's great for
    device-specific properties, these are global Lineage properties
    which are descriptive of the system side, so move them to the list
    that gets populated into /system/build.prop.
    
    Change-Id: I8bb3d0fe13188478d4f7dedc306740a40f44a3e8

--- config/common.mk
+++ config/common.mk
@@ -3,14 +3,14 @@ PRODUCT_BRAND ?= XenonHD
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     ro.com.google.clientidbase=android-google
 else
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     keyguard.no_require_sim=true \
     persist.sys.disable_rescue=true \
     ro.build.selinux=1 \
@@ -19,7 +19,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 # Thank you, please drive thru!
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
+PRODUCT_GENERIC_PROPERTIES += persist.sys.dun.override=0
 endif
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
@@ -233,12 +233,12 @@ PRODUCT_PACKAGES += \
     libffmpeg_omx \
     media_codecs_ffmpeg.xml
 
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     media.sf.omx-plugin=libffmpeg_omx.so \
     media.sf.extractor-plugin=libffmpeg_extractor.so
 
 # Media
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_GENERIC_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
 
 # These packages are excluded from user builds
