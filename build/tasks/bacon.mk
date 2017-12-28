fdf2f37af16859b5a30a88c299ec7b881390581b
Daniel Sikorski <dadi.sikorski@gmail.com>
2017-12-28 20:21:31 +0100

    xenonhd: the disclaimer returns
    Change-Id: Iabc8a87cccb0e979be435754dd2eb2bd43e75541

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -31,4 +31,5 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	@echo "md5: $(shell cat $(XENONHD_TARGET_PACKAGE).md5sum | awk '{ print $$1 }')"
 	@echo "size: $(shell ls -lah $(XENONHD_TARGET_PACKAGE) | awk '{ print $$5 }')"
 	@echo -e "==================================================\n"
+	@echo -e "Attention! Publishing (UN)OFFICIAL builds is NOT ALLOWED yet!\n"
 	$(hide) jack-admin stop-server
