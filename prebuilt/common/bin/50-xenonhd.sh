0a3388d79c27e0bdd6a1a328a0c308a58ca8e08b
axxx007xxxz <mattiabrsrn7@gmail.com>
2018-02-05 18:59:53 +0100

    xenonhd: prebuilt: bin: Update for Android 8.1
    Change-Id: I9f9c2bc40dee80c95d36b2857b3fba4a1f8e1219

--- prebuilt/common/bin/50-xenonhd.sh
+++ prebuilt/common/bin/50-xenonhd.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 #
 # /system/addon.d/50-xenonhd.sh
-# During a XenonHD 8.0 upgrade, this script backs up /system/etc/hosts,
+# During a XenonHD 8.1 upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
 
