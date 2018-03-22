3117c9371d13e22e4a82639f85fb4a3c9b431acf
makorn645 <makornthawateugeneemery@gmail.com>
2018-03-22 19:25:54 +0530

    vendorsetup: Rebrand Z017 to zenfone3
    Change-Id: Ica21f2c61a77b036bed7b191b3b1b9d138a3cc14

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,4 +7,4 @@ add_lunch_combo xenonhd_m8-userdebug
 add_lunch_combo xenonhd_marlin-userdebug
 add_lunch_combo xenonhd_mido-userdebug
 add_lunch_combo xenonhd_potter-userdebug
-add_lunch_combo xenonhd_Z017-userdebug
+add_lunch_combo xenonhd_zenfone3-userdebug
