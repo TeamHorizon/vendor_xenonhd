2a2e92b7c46c6f58d96d420a6797bd6cc563f8c4
Joey <joey@lineageos.org>
2018-03-25 18:11:51 +0200

    Make Cyan[...] great again!
    Change-Id: Iac2b7ccc5a3a26a36d5651923011064b3363b156
    Signed-off-by: Joey <joey@lineageos.org>

--- config/common.mk
+++ config/common.mk
@@ -148,6 +148,7 @@ PRODUCT_PACKAGES += \
     LineageBlackAccent \
     LineageBlueAccent \
     LineageBrownAccent \
+    LineageCyanAccent \
     LineageGreenAccent \
     LineageOrangeAccent \
     LineagePinkAccent \
