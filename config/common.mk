d400e9721d013fa2fad639a54f88e20ab1785bb0
Daniel Sikorski <dadi.sikorski@gmail.com>
2018-05-24 16:06:26 +0200

    xenonhd: fix typo

--- config/common.mk
+++ config/common.mk
@@ -79,7 +79,7 @@ PRODUCT_COPY_FILES += \
 
 # This is XenonHD!
 PRODUCT_COPY_FILES += \
-    vendor/xenonhd/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml
+    vendor/xenonhd/config/permissions/org.lineageos.android.xml:system/etc/permissions/org.lineageos.android.xml \
     vendor/xenonhd/config/permissions/privapp-permissions-lineage.xml:system/etc/permissions/privapp-permissions-lineage.xml
 
 # Include XenonHD audio files
