58f27564e74c18695f09ac1539ce062d6205616f
Aaron Kling <webgeek1234@gmail.com>
2018-06-19 12:10:10 -0500

    ATV: Build LineageCustomizer
    Change-Id: I1a9e18516441dd513378bc5332b181b4e059e19b

--- config/common_full_tv.mk
+++ config/common_full_tv.mk
@@ -1,6 +1,8 @@
 # Inherit full common XenonHD stuff
 $(call inherit-product, vendor/xenonhd/config/common_full.mk)
 
-PRODUCT_PACKAGES += AppDrawer
+PRODUCT_PACKAGES += \
+    AppDrawer \
+    LineageCustomizer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/tv
