LineageOS/android_vendor_lineage

lineage: Make sure to consider our product makefiles
Change-Id: Ia0c7281791b37fff5fce9c62b3f0ba56c35d45ba

build/target/product/AndroidProducts.mk
@@ -1,0 +1,20 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_arm.mk \
+    $(LOCAL_DIR)/lineage_arm64.mk \
+    $(LOCAL_DIR)/lineage_x86.mk

TeamHorizon/vendor_xenonhd

build/target/product/AndroidProducts.mk
@@ -1,0 +1,20 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/xenonhd_arm.mk \
+    $(LOCAL_DIR)/xenonhd_arm64.mk \
+    $(LOCAL_DIR)/xenonhd_x86.mk