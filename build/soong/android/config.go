e48dd76b4ffcdbece9bb36a776f8bef55c4bce78
dadi11 <dadi.sikorski@gmail.com>
2018-09-12 20:41:54 +0200

    soong: refactor soong variables to Xenon (5/5)
    Change-Id: I3285a012223e122d0b15d69f653d0fc658b19d8f

--- build/soong/android/config.go
+++ build/soong/android/config.go
@@ -1,7 +1,7 @@
 package android
 
 // Global config used by Lineage soong additions
-var XenonhdConfig = struct {
+var XenonConfig = struct {
 	// List of packages that are permitted
 	// for java source overlays.
 	JavaSourceOverlayModuleWhitelist []string
