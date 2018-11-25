82d3417b47e7d5c8acdfaad3cf2a9cf026213bcd
Albert I <krascgq@outlook.co.id>
2018-11-25 13:35:59 +0800

    xenonhd: Introduce (legacy) release-keys ROM signing hack
    Turns out that not every single machine is happy about signing created
    target files package immediately as some will get out-of-memory error(s)
    in RAM as soon as the package is being created.
    
    Introduce a hack that immediately signs all targets with a provided,
    "fake" test-keys to workaround this. It's not recommended to enable this
    hack unless you're owning one of those unlucky machines.
    
    Change-Id: Ie8b65c49d3a83811c7d1c9342254e7f0fa75d884
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- config/release_keys.mk
+++ config/release_keys.mk
@@ -1,6 +1,6 @@
 # Whether using or not release-keys for building
-# Copyright (C) 2018 KudProject Development
-# Copyright (C) 2018 XenonHD Project
+# Copyright (C) 2018-2019 KudProject Development
+# Copyright (C) 2018-2019 XenonHD Project
 # SPDX-License-Identifier: Apache-2.0
 
 # Don't modify; path where release-keys are hosted
@@ -11,7 +11,16 @@ ifneq ($(filter Experimental Official,$(OTA_TYPE)),)
     # Whether release-keys path is exist or not
     ifneq ($(wildcard $(RELEASE_KEYS_PATH)),)
         # Exists, use them
-        USE_RELEASE_KEYS := true
+        ifeq ($(USE_RELEASE_KEYS_SIGNING_HACK),true)
+            # Using signing hack
+            PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEYS_PATH)/testkey
+
+            # Clear this to avoid unexpected behavior
+            USE_RELEASE_KEYS :=
+        else
+            # Default
+            USE_RELEASE_KEYS := true
+        endif
     else
         # Doesn't exist, fall back to test-keys
         $(warn $(RELEASE_KEYS_PATH) doesn't exist. Continuing with test-keys...)
