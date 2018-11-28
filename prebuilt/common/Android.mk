vansh123456/android_vendor_revengeui

Use SoundPickerPrebuilt instead of AOSP
* Taken from taimen-opm1.171019.013

app/Android.mk
@@ -25,6 +25,16 @@ LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := SoundPickerPrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := SoundPickerPrebuilt/SoundPickerPrebuilt.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := WallpapersUsTwo
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := WallpapersUsTwo/WallpapersUsTwo.apk

TeamHorizon/vendor_xenonhd

Change-Id: I834c831ad98c4acf62889176a3f4f2f6463666fe
Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

prebuilt/common/Android.mk
@@ -16,3 +16,13 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SoundPickerPrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := app/SoundPickerPrebuilt.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)