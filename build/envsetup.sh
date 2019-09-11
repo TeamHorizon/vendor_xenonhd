c39ad1bea82b59316f01725cd02c84bf6a5444be
Bruno Martins <bgcngm@gmail.com>
2019-09-11 21:48:43 +0100

    envsetup: Kill Jack args export
    Jack is dead since O.
    
    Change-Id: I46e2102de025cbe141fb08f0edfb7d24ee3b7e91

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -996,9 +996,4 @@ if [ -d $(gettop)/prebuilts/snapdragon-llvm/toolchains ]; then
     esac
 fi
 
-# Android specific JACK args
-if [ -n "$JACK_SERVER_VM_ARGUMENTS" ] && [ -z "$ANDROID_JACK_VM_ARGS" ]; then
-    export ANDROID_JACK_VM_ARGS=$JACK_SERVER_VM_ARGUMENTS
-fi
-
 export OTA_DIR=$(get_build_var OUT_DIR)
